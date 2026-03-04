1366545840281a7e66c430c964327582bb3d5f65
S Prasanna Venketesh <prasanna.prodesigns@gmail.com>
2026-03-04 13:50:35 +0530

    images

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,30 +1,30 @@
 // Wedding images
-import w1 from "../assets/portfolio/wedding/w1.jpg";
-import w2 from "../assets/portfolio/wedding/w2.jpg";
-import w3 from "../assets/portfolio/wedding/w3.jpg";
-import w4 from "../assets/portfolio/wedding/w4.jpg";
-import w5 from "../assets/portfolio/wedding/w5.jpg";
-import w6 from "../assets/portfolio/wedding/w6.jpg";
+import w1 from "../assets/portfolio/wedding/w1w.jpg";
+import w2 from "../assets/portfolio/wedding/w2w.jpg";
+import w3 from "../assets/portfolio/wedding/w3w.jpg";
+import w4 from "../assets/portfolio/wedding/w4w.jpg";
+import w5 from "../assets/portfolio/wedding/w5w.jpg";
+import w6 from "../assets/portfolio/wedding/w6w.jpg";
 
 // Event images
 import e1 from "../assets/portfolio/events/e1.jpg";
-import e2 from "../assets/portfolio/events/e2.jpg";
-import e3 from "../assets/portfolio/events/e3.jpg";
-import e4 from "../assets/portfolio/events/e4.jpg";
-import e5 from "../assets/portfolio/events/e5.jpg";
-import e6 from "../assets/portfolio/events/e6.jpg";
+import e2 from "../assets/portfolio/events/e2e.jpg";
+import e3 from "../assets/portfolio/events/e3e.jpg";
+import e4 from "../assets/portfolio/events/e4e.jpg";
+import e5 from "../assets/portfolio/events/e5e.jpg";
+import e6 from "../assets/portfolio/events/e6e.jpg";
 
 // Candid images
-import c1 from "../assets/portfolio/candid/c1.jpg";
-import c2 from "../assets/portfolio/candid/c2.jpg";
-import c4 from "../assets/portfolio/candid/c3.jpg";
-import c5 from "../assets/portfolio/candid/c4.jpg";
-import c6 from "../assets/portfolio/candid/c3.jpg";
-import c3 from "../assets/portfolio/candid/c6.jpg";
-import c7 from "../assets/portfolio/candid/c7.jpg";
-import c8 from "../assets/portfolio/candid/c8.jpg";
-import c9 from "../assets/portfolio/candid/c9.jpg";
-import c10 from "../assets/portfolio/candid/c10.jpg";
+import c1 from "../assets/portfolio/candid/c1c.jpg";
+import c2 from "../assets/portfolio/candid/c2c.jpg";
+import c4 from "../assets/portfolio/candid/c3c.jpg";
+import c5 from "../assets/portfolio/candid/c4c.jpg";
+import c6 from "../assets/portfolio/candid/c3c.jpg";
+import c3 from "../assets/portfolio/candid/c6c.jpg";
+import c7 from "../assets/portfolio/candid/c7c.jpg";
+import c8 from "../assets/portfolio/candid/c8c.jpg";
+import c9 from "../assets/portfolio/candid/c9c.jpg";
+import c10 from "../assets/portfolio/candid/c10c.jpg";
 
 export const portfolioData = {
   Wedding: [w1, w2, w3, w4, w5, w6],
